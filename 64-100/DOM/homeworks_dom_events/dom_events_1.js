59ba2e0a677b94cd86eb783d689b02469bda6c73
Wo1fgarr <cotedor13@gmail.com>
2023-01-25 23:46:41 +0300

    write some  code challenge in github222567

--- 64-100/DOM/homeworks_dom_events/dom_events_1.js
+++ 64-100/DOM/homeworks_dom_events/dom_events_1.js
@@ -1,15 +1,18 @@
-// 1 get text if click button
+// 1 Output text from the data- attribute of an element
 const btn_msg = document.querySelector('#btn-msg');
 
 console.log(btn_msg.dataset.text);
 btn_msg.addEventListener('click', e => {
   alert(e.target.dataset.text);
+
+  /* alternative */
   // alert(btn.dataset.text);
   // alert(this.dataset.text);
   // alert(e.currentTarget.dataset.text);
 })
 
-// 2 hover mouse
+// 2 Paint the button red when hovering over the mouse 
+//   and return the original style when the mouse was removed from the button
 btn_msg.addEventListener('mouseover', e => {
   e.target.classList.add('red_button');
 })
@@ -18,27 +21,31 @@ btn_msg.addEventListener('mouseout', e => {
   e.target.classList.remove('red_button');
 })
 
-// 3 show any clicked tag
+// 3 Set in textContent the name of the tag that has now been clicked
 const tagTextContainer = document.getElementById('tag');
   document.body.addEventListener('click', (e) => {
     tagTextContainer.textContent = `Tag: ${e.target.nodeName}`;
   });
+
+/* alternative */
 // const allDoc = document.querySelector('html');
 // allDoc.addEventListener('click', e => {
 //   console.log(e.target);
 // })
 
 
-// 4 add li item with children count number
+// 4 Add a new item to the list by clicking the button with its serial number
 const btn_gen = document.querySelector('#btn-generate');
+const ul = document.querySelector('ul');
 btn_gen.addEventListener('click', e => {
-  const ul = document.querySelector('ul');
   let ulChildCount = ul.childElementCount;
   const li = document.createElement('li');
+
   li.textContent = `Item ${ulChildCount + 1}`;
   ul.insertAdjacentElement('beforeend', li);
 })
 
+/* alternative */
 // const generateBtn = document.getElementById('btn-generate');
 //   const list = document.querySelector('ul');
 //   function generateNewItem(e) {
