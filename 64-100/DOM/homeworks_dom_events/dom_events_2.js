6a3d699af6eb029aedf9f9a188b2f1ceac9614ba
Wo1fgar <cotedor13@gmail.com>
2023-01-27 20:09:37 +0300

    add some code22

--- 64-100/DOM/homeworks_dom_events/dom_events_2.js
+++ 64-100/DOM/homeworks_dom_events/dom_events_2.js
@@ -4,14 +4,31 @@ menu.addEventListener('click', e => {
   const item = e.target.nextElementSibling;
   const menuChildrenList = Array.from(menu.children);
 
-  console.log(item);
+  // console.log(item);
+  // console.log(menuChildrenList);
+  let i = 0;
+  while(i <= 2) {
+    console.log(menuChildrenList[0].querySelector('.dropdown-menu'));
+  }
+
   
-  menuChildrenList.forEach( elem => {
-    let nextMenuSibling = elem.nextElementSibling;
-    console.log(nextMenuSibling);
-    console.log(nextMenuSibling === item);
-    if (nextMenuSibling === item) {
-      nextMenuSibling.classList.toggle('d-none')
-    }
-  })
+
+  // menuChildrenList.forEach( elem => {
+  //   // console.log(elem);
+  //   let nextMenuSibling = elem.querySelector('.dropdown-menu');
+  //   console.log(nextMenuSibling);
+  //   // console.log(nextMenuSibling === item);
+  //   if (item === nextMenuSibling && nextMenuSibling.classList.contains('d-none')) {
+  //     nextMenuSibling.classList.remove('d-none');
+  //     if (!item === nextMenuSibling && !nextMenuSibling.classList.contains('d-none')) {
+  //       nextMenuSibling.classList.remove('d-none');
+  //     }
+  //   }
+  //   else if (item === nextMenuSibling && !nextMenuSibling.classList.contains('d-none')) {
+  //     nextMenuSibling.classList.add('d-none');
+  //   }
+  //   else if (!item === nextMenuSibling && !nextMenuSibling.classList.contains('d-none')) {
+  //     nextMenuSibling.classList.add('d-none');
+  //   }
+  // })
 })
