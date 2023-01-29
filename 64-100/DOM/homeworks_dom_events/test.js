d64d09aef4ed21495123a90c99854f42daebeae4
Wo1fgar <cotedor13@gmail.com>
2023-01-29 23:41:17 +0300

    change background color of element_without comments

--- 64-100/DOM/homeworks_dom_events/test.js
+++ 64-100/DOM/homeworks_dom_events/test.js
@@ -1,22 +1,12 @@
 const div = document.querySelector('div');
 const bg_color = div.classList.add('bg-color');
 const setBgColorProperty = document.querySelector('.bg-color');
-// setBgColorProperty.style.background = 'red';
 
 colors = ['red', 'blue', 'olive', 'orange', 'pink', 'yellow', 'green', 'gray', 'aqua', 'brown'];
-
-// let rand = Math.round(Math.random() * 10);
-// console.log(rand);
-// let randomColor = colors[rand];
-
 
 function firstfunc(arr, fn = 'noColor', someClass) {
   return someClass.style.background = `${fn(arr)}`;
 }
-
-// function firstfunc(arr, fn = 'noColor') {
-//   return `New color: ${fn(arr)}`;
-// }
 
 function getRand() {
   let result = Math.round((Math.random() * 10));
@@ -24,13 +14,7 @@ function getRand() {
   return result;
 }
 function handler1(arr) {
-  // let rand = Math.round(Math.random() * 10);
   let rand = getRand();
-  // console.log(rand);
-  // if ( rand === 10){
-  //   return 'no Color';
-  // }
-  // return `${arr[rand]} index: ${rand}`;
   return arr[rand];
 }
 
