a559067a4f55a70e7e53d354bca17c8cb4a4edb5
Wo1fgarr <cotedor13@gmail.com>
2023-04-14 22:01:32 +0300

    freecodecamp commit

---
+++ 121-200/JS from beginner to professional/StartJS/2/src/js/app.js
@@ -0,0 +1,7 @@
+let a = 10; // 10 - is a value
+/* This is a multistring comment */
+
+let userName = prompt("Hi! What is your name?");
+console.log(userName);
+
+console.log(Math.round(Math.random() * 10));
